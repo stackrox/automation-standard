77191ef180668106835c2460558bc0a6fa0472bb
Josh Komoroske <josh@stackrox.com>
2020-02-28 13:40:34 -0800

    Better application contents

--- application.go
+++ application.go
@@ -5,13 +5,36 @@ import (
 	"time"
 )
 
+// Application represents the full configuration for a runnable automation
+// flavor.
 type Application struct {
-	Name        string
+	// Name is a human-readable name for this application.
+	//
+	// Example: "Example Cluster"
+	Name string
+
+	// Description is a human-readable Description for this application.
+	//
+	// Example: "Provisions an Example cluster"
 	Description string
-	Homepage    string
-	Version     string
-	Create      ActionConfiguration
-	Destroy     ActionConfiguration
+
+	// Homepage is a URL that links to this application's GitHub project.
+	//
+	// Example: "https://github.com/stackrox/example-cluster-automation"
+	Homepage string
+
+	// Version is the specific version of this application.
+	//
+	// Example: "v1.2.3"
+	Version string
+
+	// Create is the extended configuration for this applications "create"
+	// action.
+	Create ActionConfiguration
+
+	// Destroy is the extended configuration for this applications "destroy"
+	// action.
+	Destroy ActionConfiguration
 }
 
 type ActionConfiguration struct {
